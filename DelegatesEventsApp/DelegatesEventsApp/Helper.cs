bd188085dc0aa8dec652c77772456301a4c9d316
Azamat Davletbaev <azamat.davletbaev@gmail.com>
2024-02-19 20:44:37 +0500

    2

---
+++ DelegatesEventsApp/DelegatesEventsApp/Helper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesEventsApp
+{
+    public static class Helper
+    {
+        public static T GetMax<T>(this IEnumerable collection, Func<T, float> convertToNumber) where T : class
+        {
+
+
+            return default;
+        }
+
+    }
+}
